7a6d890491f4ff855bc84a8b639d88638ee287f3
shobeedev <choubimohammed99@gmail.com>
2025-10-18 11:51:50 +0100

    ex01

---
+++ Programming Using C++ - Level 1/ex01/output.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   output.cpp                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/18 11:08:58 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/18 11:32:53 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+int main()
+{
+	std::cout << "Welocome to our course\n\n";
+	std::cout << "Home page" << std::endl << std::endl;
+	std::cout << "Mohammed Choubi!\n";
+	std::cout << "We\n" << "are\n" << "the\n" << "best!!\n";
+	std::cout << "the sum of this nbrs 15 + 82 + 96 + 14 is " << 15+82+96+14;
+	return 0;
+
+}
