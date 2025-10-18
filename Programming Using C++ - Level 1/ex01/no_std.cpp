c3205d4488d0e40d6eca6af1af725a5e4018526b
shobeedev <choubimohammed99@gmail.com>
2025-10-18 12:38:47 +0100

    No Std

---
+++ Programming Using C++ - Level 1/ex01/no_std.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   no_std.cpp                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/18 12:36:11 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/18 12:38:06 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Now we are using just cout, this is HelFull.\n";
+}
