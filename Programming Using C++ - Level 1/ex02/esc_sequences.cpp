079181b06455dbe9be6cc220fd820544457d47f5
shobeedev <choubimohammed99@gmail.com>
2025-10-18 18:04:27 +0100

    ex02

---
+++ Programming Using C++ - Level 1/ex02/esc_sequences.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   esc_sequences.cpp                                      |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/18 17:48:00 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/18 17:56:21 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//ex00
+	cout << "\a\n";
+
+	//ex01
+	cout << "Dear Sir \\Madam,\n\nHow Are you ?\n\nMy name is \"Mohammed\", nice to meet you.\n";
+
+	//ex02
+	cout << "ali\tahmed\tlira\nfarid\thamza\tmouad\n";
+
+	return 0;
+
+
+
+}
