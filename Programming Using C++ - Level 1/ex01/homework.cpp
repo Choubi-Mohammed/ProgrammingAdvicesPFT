70a25dcb9dcd901a0b90156ec4d53db359116d4e
Choubi-Mohammed <moha.saoud22@gmail.com>
2025-10-18 11:58:53 +0100

    ex

---
+++ Programming Using C++ - Level 1/ex01/homework.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   homework.cpp                                           |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/18 11:52:17 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/18 11:57:49 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "********************************************\n";
+	std::cout << "Name : Mohammed Choubi.\n";
+	std::cout << "Age : 21 Years.\n";
+	std::cout << "City : Kelaa Sraghna.\n";
+	std::cout << "Country : Morocco.\n";
+	std::cout << "********************************************\n";
+
+	return 0;
+
+}
