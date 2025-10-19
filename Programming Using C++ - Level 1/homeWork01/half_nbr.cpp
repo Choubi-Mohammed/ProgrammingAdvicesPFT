8378ed7b2ac199d318a21575850ae5cf9bf3ca0b
Choubi-Mohammed <moha.saoud22@gmail.com>
2025-10-19 17:58:33 +0100

    01

---
+++ Programming Using C++ - Level 1/homeWork01/half_nbr.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   half_nbr.cpp                                           |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/19 17:55:08 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/19 17:57:49 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nbr;
+	cout << "Please entre the number :";
+	cin >> nbr;
+	cout << "Half of " << nbr << " is " << nbr/2 << endl;
+
+	return 0;
+}
