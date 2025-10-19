89a29e5e81c5abde5116252e86bc3a306cd0d9aa
shobeedev <choubimohammed99@gmail.com>
2025-10-19 18:30:54 +0100

    06

---
+++ Programming Using C++ - Level 1/homeWork01/traingle_erae.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   traingle_erae.cpp                                      |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/19 18:26:33 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/19 18:29:56 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,h;
+	cout << "please entre a :";
+	cin >> a;
+	cout << "please entre h :";
+	cin >> h;
+
+	int Erea = (a/2)*h;
+	cout << "Traingle Erea is : " << Erea << endl;
+
+	return 0;
+}
