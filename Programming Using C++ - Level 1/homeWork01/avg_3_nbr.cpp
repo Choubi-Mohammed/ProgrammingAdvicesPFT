8c8c4f1807bc89f0d6c3bb8a0954e59bb2b0ea18
Choubi-Mohammed <moha.saoud22@gmail.com>
2025-10-19 18:12:44 +0100

    02-03

---
+++ Programming Using C++ - Level 1/homeWork01/avg_3_nbr.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   avg_3_nbr.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/19 18:04:31 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/19 18:11:03 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m1,m2,m3;
+	cout << "please entre the Mark1 :";
+	cin >> m1;
+	cout << "please entre the Mark2 :";
+	cin >> m2;
+	cout << "please entre the Mark3 :";
+	cin >> m3;
+
+	cout << "Avrage of these 3 numbers is : " << (m1+m2+m3)/3 << endl;
+
+	return 0;
+}
---
+++ Programming Using C++ - Level 1/homeWork01/sum_3_nbr.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   sum_3_nbr.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/19 17:59:14 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/19 18:03:31 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n1,n2,n3;
+	cout << "Pleas entre N1 :";
+	cin >> n1;
+	cout << "Please entre N2 :";
+	cin >> n2;
+	cout << "Please entre N3 :";
+	cin >> n3;
+
+	cout << "the sum of 3 numbers is :" << n1+n2+n3 << endl;
+	return 0;
+
+}
