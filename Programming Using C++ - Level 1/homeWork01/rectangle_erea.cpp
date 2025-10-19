3ca5fd120d16c1cf4d89440919cef1980b5cd092
Choubi-Mohammed <moha.saoud22@gmail.com>
2025-10-19 18:25:45 +0100

    05

---
+++ Programming Using C++ - Level 1/homeWork01/rectangle_erea.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   rectangle_erea.cpp                                     |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/19 18:22:04 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/19 18:25:08 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout << "please entre a :";
+	cin >> a;
+	cout << "please entre b :";
+	cin >> b;
+	int erea = a * b;
+	cout << "Rectangle Erea is : " << erea << endl;
+
+	return 0;
+}
