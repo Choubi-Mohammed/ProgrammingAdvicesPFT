09c6d6bfd75148f822c17100684ef87917606243
shobeedev <choubimohammed99@gmail.com>
2025-10-25 17:32:27 +0100

    ex06

---
+++ Programming Using C++ - Level 1/ex06/art_oper.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   art_oper.cpp                                           |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/25 17:29:12 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/25 17:31:53 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A = 10;
+	int B = 20;
+	A++;
+	++B;
+
+	cout << " A = " << A << endl;
+	cout << " B = " << B << endl;
+
+	return 0;
+
+
+}
