4886670fc19aa48ceda94ac2a1ba0cb012134708
shobeedev <choubimohammed99@gmail.com>
2025-10-27 10:45:32 +0100

    08

---
+++ Programming Using C++ - Level 1/homeWork02/pow_2_3_4.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   pow_2_3_4.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/27 10:39:44 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/27 10:44:44 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	int a;
+	cout << "please entre number : ";
+	cin >> a;
+	cout << "power of 2 : " << pow(a,2) << endl;
+	cout << "power of 3 : " << pow(a,3) << endl;
+	cout << "power of 4 : " << pow(a,4) << endl;
+
+	return 0;
+}
