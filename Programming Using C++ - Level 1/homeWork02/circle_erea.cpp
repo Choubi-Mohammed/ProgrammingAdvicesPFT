d2525e44f69c780971985eabefda789dfdc19999
Choubi-Mohammed <moha.saoud22@gmail.com>
2025-10-27 09:51:41 +0100

    02

---
+++ Programming Using C++ - Level 1/homeWork02/circle_erea.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   circle_erea.cpp                                        |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/26 20:13:02 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/27 09:50:40 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int r;
+	float PI = 3.14;
+	cout << "please entre the number :";
+	cin >> r;
+	float res =  PI*pow(r,2);
+	cout << "the res is :" << res << endl;
+
+	return 0;
+
+
+}
