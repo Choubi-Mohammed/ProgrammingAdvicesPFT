3cf4ef18dc5255e9205704bfa13647b883738e31
Choubi-Mohammed <moha.saoud22@gmail.com>
2025-10-30 17:16:57 +0100

    01

---
+++ Programming Using C++ - Level 1/homeWork03/print_name.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   print_name.cpp                                         |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/10/30 17:13:28 by shobeedev               shobee4ever      */
+/*   Updated: 2025/10/30 17:16:36 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+void print_name(string name)
+{
+	cout << "My Name is : " << name << endl;
+}
+
+int main()
+{
+	string name  = "Mohammed choubi";
+	print_name(name);
+
+	return 0;
+
+}
