49efbdeba05a1a31804aa91c1e8451055be9301e
shobeedev <moha.saoud22@gmail.com>
2025-11-04 17:29:13 +0100

    First 10 natural number

---
+++ Programming Using C++ - Level 1/ForLoopExercises/first_10_Natural_nbr.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   first_10_Natural_nbr.cpp                               |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/04 17:26:00 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/04 17:28:43 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int i = 1;i <= 10 ;i++)
+	{
+		cout << i << " ";
+	}
+
+	return 0;
+}
