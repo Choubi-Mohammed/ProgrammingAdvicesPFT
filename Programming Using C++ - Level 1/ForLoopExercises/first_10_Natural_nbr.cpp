be44580cc79db301374bd17b7aee7a200df1c185
shobeedev <moha.saoud22@gmail.com>
2025-11-04 17:32:52 +0100

    First 10 natural number

--- Programming Using C++ - Level 1/ForLoopExercises/first_10_Natural_nbr.cpp
+++ Programming Using C++ - Level 1/ForLoopExercises/first_10_Natural_nbr.cpp
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2025/11/04 17:26:00 by shobeedev               shobee4ever      */
-/*   Updated: 2025/11/04 17:28:43 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2025/11/04 17:32:34 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@ using namespace std;
 
 int main()
 {
+	cout << "The natural numbers are:\n";
 	for(int i = 1;i <= 10 ;i++)
 	{
 		cout << i << " ";
