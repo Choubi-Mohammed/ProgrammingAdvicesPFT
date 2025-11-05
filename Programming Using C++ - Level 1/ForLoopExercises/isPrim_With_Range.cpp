ff74061e6525f7a6980c5f54c64f191021749e4c
shobeedev <moha.saoud22@gmail.com>
2025-11-05 16:54:07 +0100

    Prime number with range

--- Programming Using C++ - Level 1/ForLoopExercises/isPrim_With_Range.cpp
+++ Programming Using C++ - Level 1/ForLoopExercises/isPrim_With_Range.cpp
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2025/11/05 12:53:39 by shobeedev               shobee4ever      */
-/*   Updated: 2025/11/05 13:23:25 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2025/11/05 16:53:40 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,15 @@ int main()
 	cin >> startNbr;
 	cout << "please entre endNbr :";
 	cin >> endNbr;
-
+	int counter = 0;
+	cout << "the Prime number between " << startNbr << " and " << endNbr << " are : \n";
 	for(int start = startNbr;start <= endNbr;start++)
 	{
 		if (start == 2)
+		{
+			counter = 1;
 			cout << start << " ";
+		}
 		for(int startTwo = 2;startTwo < start; startTwo++ )
 		{
 			if(start % startTwo == 0)
@@ -32,9 +36,13 @@ int main()
 			if(startTwo == start - 1)
 			{
 				cout << start << " ";
+				counter++;
 				break;
 			}
 
 		}
 	}
+	cout << "\nThe total number of prime numbers between " << startNbr << " and " << endNbr << " is :" << counter << endl;
+
+
 }
