7afaf9ace17ff8b7769e50b5aa49bc6b2ab62e29
shobeedev <moha.saoud22@gmail.com>
2025-11-06 17:02:04 +0100

    Print a to z

---
+++ Programming Using C++ - Level 1/homeWork07/Print_A_Z.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Print_A_Z.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/06 16:51:40 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/06 16:58:15 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Print A to Z :\n";
+	for(int i = 65;i <=90;i++)
+	{
+		cout <<(char) i << " ";
+	}
+
+	cout << "\nPrint a to z:\n";
+	for(int i = 97;i <=122; i++)
+	{
+		cout << (char)i << " ";
+	}
+}
