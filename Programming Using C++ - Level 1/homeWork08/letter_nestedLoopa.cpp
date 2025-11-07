6d9db79dce06a2b5705bd1f2df7cb88e81c44c0d
shobeedev <moha.saoud22@gmail.com>
2025-11-07 18:36:25 +0100

    Letters

---
+++ Programming Using C++ - Level 1/homeWork08/letter_nestedLoopa.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   letter_nestedLoopa.cpp                                 |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/07 18:26:26 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/07 18:35:00 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int i=65;i<= 90;i++)
+	{
+		for(int j=65;j<=90;j++)
+		{
+			cout << (char) i;
+			cout << (char) j;
+			cout << "\n";
+		}
+
+	}
+}
