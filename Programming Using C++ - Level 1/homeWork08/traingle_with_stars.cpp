dbd7317670c76ad5ee71d20d4915f99e4a8ec7a5
shobeedev <moha.saoud22@gmail.com>
2025-11-07 18:25:22 +0100

    traingle with starts

---
+++ Programming Using C++ - Level 1/homeWork08/traingle_with_stars.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   traingle_with_stars.cpp                                |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/07 18:23:05 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/07 18:24:45 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int i = 1;i <= 10;i++)
+	{
+		for(int j = 1;j<=10;j++)
+			cout << "*";
+		cout << "\n";
+	}
+
+	return 0;
+}
