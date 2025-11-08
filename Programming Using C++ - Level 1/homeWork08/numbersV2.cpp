c86526f820a94bf23272446c5330829d6aeb34da
shobeedev <moha.saoud22@gmail.com>
2025-11-08 10:08:26 +0100

    Numbers

---
+++ Programming Using C++ - Level 1/homeWork08/numbersV2.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   numbersV2.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/08 10:06:16 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/08 10:07:45 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int i =	1;i <= 10;i++)
+	{
+		for(int j = 1;j <= i;j++)
+			cout << j << " ";
+		cout << "\n";
+	}
+}
