f9f2453f3092a878af02f156b8fff64288093480
shobeedev <moha.saoud22@gmail.com>
2025-11-08 19:23:50 +0100

    While loop letters

---
+++ Programming Using C++ - Level 1/homeWork09/letters_A_Z.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   letters_A_Z.cpp                                        |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/08 19:21:48 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/08 19:23:21 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i =65;
+	while(i <= 90)
+	{
+		cout << (char) i << " ";
+		i++;
+	}
+}
