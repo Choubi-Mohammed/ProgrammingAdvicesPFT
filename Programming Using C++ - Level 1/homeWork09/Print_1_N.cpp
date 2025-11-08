56a8d57477316e5a560b80502bd0326316a20e7d
shobeedev <moha.saoud22@gmail.com>
2025-11-08 15:12:12 +0100

    While loop Print 1 to N

--- Programming Using C++ - Level 1/homeWork09/Print_1_N.cpp
+++ Programming Using C++ - Level 1/homeWork09/Print_1_N.cpp
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2025/11/08 15:06:19 by shobeedev               shobee4ever      */
-/*   Updated: 2025/11/08 15:07:49 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2025/11/08 15:11:36 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@ using namespace std;
 
 int main()
 {
+	int Number;
+	cout << "please entre A  number : ";
+	cin >> Number;
+	while(Number < 0)
+	{
+		cout << "please entre a positive number : ";
+		cin >> Number;
+	}
+
 	int i =1;
-	while(i <= 10)
+	while(i <= Number)
 	{
 		cout << i << "\n";
 		i++;
