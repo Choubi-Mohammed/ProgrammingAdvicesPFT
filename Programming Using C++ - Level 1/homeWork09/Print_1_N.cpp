6076d99f2fc35fc7d257e48b01dab13fe9051dad
shobeedev <moha.saoud22@gmail.com>
2025-11-08 15:08:33 +0100

    While loop Print 1 to N

---
+++ Programming Using C++ - Level 1/homeWork09/Print_1_N.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Print_1_N.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/08 15:06:19 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/08 15:07:49 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i =1;
+	while(i <= 10)
+	{
+		cout << i << "\n";
+		i++;
+	}
+
+	return 0;
+}
