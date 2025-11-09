30fe0a945fa21c277a68c0c7d990287d6b36e910
shobeedev <moha.saoud22@gmail.com>
2025-11-09 18:10:25 +0100

    continue statement

---
+++ Programming Using C++ - Level 1/continue_state.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   continue_state.cpp                                     |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/09 18:01:37 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/09 18:09:54 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nbr;
+	int i = 0;
+	int sum = 0;
+	while(i < 5-1)
+	{
+		cout << "ente a number " ;
+		cin >> nbr;
+		if(nbr > 50)
+			continue;
+		sum = sum +nbr;
+		i++;
+	}
+	cout << "the sum is " << sum << endl;
+}
