7a8cbf9831eb8fafef75f5db4c31145a1d7c6d1a
shobeedev <moha.saoud22@gmail.com>
2025-11-11 17:21:11 +0100

    Problem #1

--- Programming Using C++ - Level 1/SolutionsFlowChartProblem/Problem1.cpp
+++ Programming Using C++ - Level 1/SolutionsFlowChartProblem/Problem1.cpp
@@ -1,36 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                           ____    _____    */
-/*   PerfectNumberV2.cpp                                    |___ \  |___ /    */
+/*   Problem#1.cpp                                          |___ \  |___ /    */
 /*                                                            __) |   |_ \    */
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
-/*   Created: 2025/11/04 22:55:46 by shobeedev               shobee4ever      */
-/*   Updated: 2025/11/04 23:04:21 by shobeedev            tfaaty fi l3oolaa   */
+/*   Created: 2025/11/11 17:16:36 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/11 17:18:20 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 using namespace std;
 
+void PrintName(string name)
+{
+	cout << "\n Your Name is : " << name << endl;
+}
+
 int main()
 {
-	int number;
-	cout << "please entre number :";
-	cin >> number;
-	int sum=0;
-	for(int n = 1;n <= number;n++)
-	{
-		sum = 0;
-		for(int i = 1;i<= n/2;i++)
-		{
-			if (n % i == 0)
-				sum = sum +i;
+	PrintName("Mohammed Choubi");
 
-		}
-		if(sum == n)
-			cout << n << endl;
-	}
-
-
+	return 0;
 }
