87cd558e65e58cd5617a07b4060f8708c788fc89
shobeedev <moha.saoud22@gmail.com>
2025-11-15 11:00:48 +0100

    Problem #20

---
+++ Programming Using C++ - Level 1/SolutionsFlowChartProblem/Problem20.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Problem20.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/15 10:22:11 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/15 10:56:27 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+void ReadNumber(int &nbr)
+{
+	cout << "entre a number :";;
+	cin >> nbr;
+
+}
+
+int sumEvennbr_using_while(int nbr)
+{
+	int counter = 1;
+	int sum = 0;
+	cout << "\nusing while loop =>\n";
+	while(counter <= nbr)
+	{
+		if (counter % 2 == 0)
+		{
+			cout << counter << " " ;
+			sum = sum + counter;
+		}
+		counter++;
+	}
+	cout << " => ";
+	return sum;
+}
+
+int sumEvennbr_using_for(int nbr)
+{
+	int sum = 0;
+	cout << "\nusing for loop =>\n";
+	for(int i = 1;i <= nbr;i++)
+	{
+		if (i % 2 == 0)
+		{
+			cout << i << " ";
+			sum = sum + i;
+		}
+	}
+	cout << " => ";
+	return sum;
+}
+
+int sumEvennbr_using_dowhile(int nbr)
+{
+	cout << "\nusing do while loop =>\n";
+	int sum = 0;
+	int counter = 1;
+	do {
+		if(counter % 2 == 0)
+		{
+			cout << counter << " ";
+			sum = sum + counter;
+		}
+		counter++;
+	}while(counter <= nbr);
+	cout << " => ";
+	return sum;
+}
+
+int main()
+{
+	int nbr;
+	ReadNumber(nbr);
+	cout << sumEvennbr_using_while(nbr);
+	cout << sumEvennbr_using_for(nbr);
+	cout << sumEvennbr_using_dowhile(nbr);
+
+
+	return 0;
+}
