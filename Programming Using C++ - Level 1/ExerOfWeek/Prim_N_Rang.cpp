3bcc230a9f998ba8194ec7876e256c4feb35b18f
shobeedev <moha.saoud22@gmail.com>
2025-11-16 19:46:52 +0100

    Find Prime Numbers Within a Range

---
+++ Programming Using C++ - Level 1/ExerOfWeek/Prim_N_Rang.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Prim_N_Rang.cpp                                        |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/16 17:51:15 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/16 19:43:17 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+void ReadNumbers(int &from,int &to)
+{
+	cout << "frome => ";
+	cin >> from;
+	cout << "to => ";
+	cin >> to;
+}
+bool PrimOrNot(int start,int number)
+{
+	int counter = start;
+	while(counter < number)
+	{
+		if (number % counter == 0)
+			return false;
+		counter++;
+	}
+	return true;
+}
+
+void FindPrimNumber(int start,int end)
+{
+	if (start <= 1)
+		start++;
+	int counter =start;
+	while(counter <= end)
+	{
+		if (PrimOrNot(start,counter) == true)
+			cout << counter << " ";
+		counter++;
+	}
+}
+
+int main()
+{
+	int from,to;
+	ReadNumbers(from,to);
+	FindPrimNumber(from,to);
+
+	return 0;
+}
