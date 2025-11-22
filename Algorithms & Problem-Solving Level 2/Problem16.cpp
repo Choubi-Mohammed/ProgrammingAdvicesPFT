9000e13646ac9937e84da67dc58ffac06bd9a977
shobeedev <moha.saoud22@gmail.com>
2025-11-22 20:17:30 +0100

    Guess 3 Letters PassWord

---
+++ Algorithms & Problem-Solving Level 2/Problem16.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Problem16.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/11/22 19:15:02 by shobeedev               shobee4ever      */
+/*   Updated: 2025/11/22 20:04:57 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+void ReadPassWord(string &Pass)
+{
+	cout << "Please Guess 3 letters PassWord : ";
+	cin >> Pass;
+}
+void Guess_3_lett_pass(string PassWord)
+{
+	string Word = "";
+	int i = 65;
+	int counter = 0;
+	while(i <= 90)
+	{
+		int j = 65;
+		while(j <= 90)
+		{
+			int k = 65;
+			while(k <= 90)
+			{
+				counter++;
+				Word = Word + (char)i;
+				Word = Word + (char)j;
+				Word = Word + (char)k;
+
+				if(Word == PassWord)
+				{
+					cout << "Password is " << Word << endl;
+					cout << "Found after " << counter << " Trail(s)."<< endl;
+					return;
+				}
+				Word = "";
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int main()
+{
+	string Password;
+	ReadPassWord(Password);
+	Guess_3_lett_pass(Password);
+
+	return 0;
+}
