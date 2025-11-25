79c6fa4f1a23ee660c6f7d3b7fd165fde8e64060
shobeedev <moha.saoud22@gmail.com>
2025-11-25 18:42:14 +0100

    Generate Keys

--- Algorithms & Problem-Solving Level 1/Problem20.cpp
+++ Algorithms & Problem-Solving Level 1/Problem20.cpp
@@ -6,13 +6,14 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2025/11/25 17:46:39 by shobeedev               shobee4ever      */
-/*   Updated: 2025/11/25 18:06:08 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2025/11/25 18:41:22 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+
 int ReadNumber()
 {
 	int nbr;
@@ -25,51 +26,71 @@ int RandomNumber(int from,int to)
 	int RandomNumber = rand() % (to - from + 1) + from;
 	return RandomNumber;
 }
-
-void PrintCapChar()
+enum CharType {Letter = 1, CapLetter = 2,SpeChar = 3,Digit = 4};
+char RandChar(CharType type)
 {
-	int CapLetter = RandomNumber(65,90);
-	cout << (char) CapLetter;
-}
-
-void Print4Char()
-{
-	int i = 1;
-	while (i <= 4)
+	switch(type)
 	{
-		PrintCapChar();
+		case CharType::Letter:
+			{
+				return (char) RandomNumber(97,122);
+				break;
+			}
+		case CharType::CapLetter :
+			{
+				return (char) RandomNumber(65,90);
+				break;
+			}
+		case CharType::SpeChar:
+			{
+				return (char) RandomNumber(33,63);
+				break;
+			}
+		case CharType::Digit:
+			{
+				return (char) RandomNumber(48,57);
+				break;
+			}
+	}
+	return '\0';
+}
+string GenerateWord(CharType type,int len)
+{
+	string Word = "";
+	int i = 1;
+	while(i <= len)
+	{
+		Word = Word + RandChar(type);
 		i++;
 	}
+	return Word;
 }
-void Key()
+
+void GenerateKey()
 {
-	int i = 1;
-	while(i <= 4)
-	{
-		Print4Char();
-		if (i != 4)
-			cout << '-';
-		i++;
-	}
+	cout << GenerateWord(CharType::CapLetter,5) + "-";
+	cout << GenerateWord(CharType::CapLetter,5) + "-";
+	cout << GenerateWord(CharType::CapLetter,5) + "-";
+	cout << GenerateWord(CharType::CapLetter,5);
 }
-void GenerateKeys(int number)
+
+void NumberOfKeys(int number)
 {
 	int i = 1;
 	while(i <= number)
 	{
-		cout << "Key [" << i << "] : " ;
-		Key();
-		cout << endl;
+		cout << "Key [" << i <<  "] : ";
+		GenerateKey();
+		cout <<  endl;
 		i++;
 	}
+
 }
-
-
 int main()
 {
 	srand((unsigned) time(NULL));
 	int number = ReadNumber();
-	GenerateKeys(number);
+	NumberOfKeys(number);
 
 	return 0;
 
