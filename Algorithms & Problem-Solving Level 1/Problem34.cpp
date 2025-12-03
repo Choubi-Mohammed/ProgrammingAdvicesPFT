03a39f5056d6cfd46e983297ff26603b2b2675ee
shobeedev <moha.saoud22@gmail.com>
2025-12-03 19:23:57 +0100

    Check Number in Array

---
+++ Algorithms & Problem-Solving Level 1/Problem34.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Problem34.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/03 19:04:11 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/03 19:21:29 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void ReadLen(int &len)
+{
+	cout << "nter the length : ";
+	cin >> len;
+}
+
+int RandomNumber(int from,int to)
+{
+	int RandomNumber = rand() % (to - from - 1) + from;
+	return RandomNumber;
+}
+
+void FillArr(int arr[],int len)
+{
+	int i = 0;
+	while(i < len)
+	{
+		arr[i] = RandomNumber(1,100);
+		i++;
+	}
+}
+void readSearchNbr(int &SearchNbr)
+{
+	cout << "Please enter the number to search : ";
+	cin >> SearchNbr;
+}
+
+bool CheckSearchnbr(int arr[],int len,int seaarchnbr)
+{
+	int i = 0;
+	while(i < len)
+	{
+		if (arr[i] == seaarchnbr)
+			return true;
+		i++;
+	}
+	return false;
+}
+
+void PrintFoudOrNot(bool res)
+{
+	if ( res == true)
+		cout << "Yes, the number is found.\n";
+	else
+		cout << "No, The number is not found.\n";
+}
+
+
+void PrintArr(int arr[],int len)
+{
+	int i =0;
+	while(i < len)
+	{
+		cout << arr[i] << " ";
+		i++;
+	}
+	cout << "\n\n";
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int arr[100],len,seaarchn;
+	ReadLen(len);
+
+	FillArr(arr,len);
+	cout << "Element of Array : ";
+	PrintArr(arr,len);
+
+	readSearchNbr(seaarchn);
+	PrintFoudOrNot(CheckSearchnbr(arr,len,seaarchn));
+
+	return 0;
+}
