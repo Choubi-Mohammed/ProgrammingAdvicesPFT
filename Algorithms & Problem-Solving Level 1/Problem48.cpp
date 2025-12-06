7934ebb3a732033f7c6f664d3cda09d3c10de436
shobeedev <moha.saoud22@gmail.com>
2025-12-06 21:19:17 +0100

    My ceil() function

---
+++ Algorithms & Problem-Solving Level 1/Problem48.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Problem48.cpp                                          |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/06 20:58:20 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/06 21:18:32 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float RetNumber()
+{
+	float number;
+	cout << "Please enter a number : ";
+	cin >> number;
+	return number;
+}
+float getFraction(float number)
+{
+	return number - (int)number;
+}
+float tt_abs(float number)
+{
+	if(number > 0)
+		return number;
+	else if(number < 0)
+		return -number;
+	else
+		return number;
+}
+int tt_ceil(float number)
+{
+	if(tt_abs(getFraction(number)) > 0)
+	{
+		if (number > 0)
+			return ++number;
+		else
+			return number;
+	}else
+		return number;
+}
+
+int main()
+{
+	float number =RetNumber();
+	cout << "My floor() Function  : " << tt_ceil(number) << endl;
+	cout << "C++ floor() Function : " << ceil(number) << endl;
+
+	return 0;
+}
