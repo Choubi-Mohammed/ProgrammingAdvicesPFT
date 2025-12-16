c5e6df9450ed9209d5b3e6289523860cb548d9cb
shobeedev <moha.saoud22@gmail.com>
2025-12-16 17:49:24 +0100

    Creat the first lib

---
+++ Programming Using C++ - Level 2/Miscellaneous/Header_Files/mylib.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   mylib.h                                                |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/16 17:35:55 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/16 17:47:34 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+#pragma once
+#include <iostream>
+using namespace std;
+
+namespace tt_mylib
+{
+	void hello()
+	{
+		cout << "Hello World!\n";
+	}
+}
---
+++ Programming Using C++ - Level 2/Miscellaneous/Source_Files/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   main.cpp                                               |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/16 17:39:57 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/16 17:47:01 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../Header_Files/mylib.h"
+using namespace std;
+
+int main()
+{
+	tt_mylib::hello();
+	return 0;
+}
