8280b75e6e656042849044c5381b0f42e5f49ed9
shobeedev <moha.saoud22@gmail.com>
2025-12-17 21:41:30 +0100

    Bitwise OR operator

---
+++ Programming Using C++ - Level 2/Miscellaneous/Bitwise/Bitwise_OR_opr.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Bitwise_OR_opr.cpp                                     |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/17 19:42:28 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/17 21:41:11 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//exp 1 :
+	//number = 15 in binary   = 00001111;
+	//number = 17 in binary   = 00010001;
+//ila drna comp bi | binathom = 00011111;
+   //so the res of binary in decimal is = 31;
+	cout << "Result : " << (15 | 17) << endl;
+
+	//exp 2 :
+	//number = 45, binary = 00101101;
+	//number = 78, binary = 01001110;
+	//comparition         = 01101111;
+	//the res in decimal  = 111;
+	cout << "Result : " << (45 | 78) << endl;
+
+	return 0;
+}
