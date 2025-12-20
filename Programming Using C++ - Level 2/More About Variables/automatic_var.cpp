8baa91507b0e072cba39d2e49d49d27b37829381
shobeedev <moha.saoud22@gmail.com>
2025-12-20 17:06:10 +0100

    automatic varaible

---
+++ Programming Using C++ - Level 2/More About Variables/automatic_var.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   automatic_var.cpp                                      |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/20 17:03:37 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/20 17:05:45 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	auto a = 15;//type integer
+	auto b = 14.6;//type double
+	auto c = "Mohammed Choubi";//type string;
+
+	cout << a << endl
+		 << b << endl
+		 << c << endl;
+
+	return 0;
+}
