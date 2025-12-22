d9b45571b84fd9f0956142df920ced1912a0b4f3
shobeedev <moha.saoud22@gmail.com>
2025-12-22 11:07:08 +0100

    intger format

--- Programming Using C++ - Level 2/Printing & Formatting/integer_format.c
+++ Programming Using C++ - Level 2/Printing & Formatting/integer_format.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2025/12/22 10:49:25 by shobeedev               shobee4ever      */
-/*   Updated: 2025/12/22 11:00:33 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2025/12/22 11:06:51 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,13 @@ int main()
 	printf("%d + %d = %d\n",number,number_tw,number_tw+number);
 
 	//sepcific width
+	//method 1
 	printf("number = %0*d\n",2,number);
 	printf("number = %0*d\n",3,number);
 	printf("number = %0*d\n",4,number);
+	//method 2
+	printf("number = %02d\n",number);
+	printf("number = %03d\n",number);
+	printf("number = %04d\n",number);
+
 }
