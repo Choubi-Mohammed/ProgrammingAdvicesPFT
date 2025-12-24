e3192390af80a7c108b7fefbc0ce58d595c1a7fd
shobeedev <moha.saoud22@gmail.com>
2025-12-24 10:46:01 +0100

    Setw Manipulator

---
+++ Programming Using C++ - Level 2/Printing & Formatting/setw_manipulator.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   setw_manipulator.cpp                                   |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/24 10:45:23 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/24 10:45:32 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << setw(10) << 5 << endl;
+    cout << setw(10) << 123 << endl;
+    cout << setw(10) << 12345678901 << endl;
+}
+
