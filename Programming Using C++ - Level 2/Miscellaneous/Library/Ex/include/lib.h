c2d4e5936ae88ca19d6ee5133e61827969e021cb
shobeedev <moha.saoud22@gmail.com>
2025-12-24 20:57:12 +0100

    create header file

---
+++ Programming Using C++ - Level 2/Miscellaneous/Library/Ex/include/lib.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   lib.h                                                  |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/24 12:19:40 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/24 12:34:13 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+int tt_sum(int a,int b);
+bool tt_isEven(int n);
+int tt_max(int a,int b);
+int tt_fact(int nbr);
+int tt_pow(int base,int exp);
+bool tt_isPrim(int n);
+int tt_abs(int n);
