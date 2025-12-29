07bc172daab84f0479387c3a9a9b3cf82ab824ea
shobeedev <moha.saoud22@gmail.com>
2025-12-29 10:48:36 +0100

    Pointers - common mistakes

---
+++ Programming Using C++ - Level 2/Pointers/pointer_common_mistakes.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   pointer_common_mistakes.cpp                            |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/29 10:45:35 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/29 10:48:02 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, * p;
+
+	// Wrong!
+	// p is an address but x is not
+	p = x;
+
+	// Correct!
+	// p is an address and so is &x
+	p = &x;
+
+	// Wrong!
+	// &x is an address
+	// *p is the value stored in &x
+	*p = &x;
+
+	// Correct!
+	// *p is the value and so x
+	*p = x;
+
+	return 0;
+}
