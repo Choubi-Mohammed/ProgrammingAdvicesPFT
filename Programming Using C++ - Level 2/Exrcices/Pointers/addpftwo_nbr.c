3c2dc37e1a6a1aabfc706f536d34e7da8a5e6a8a
shobeedev <moha.saoud22@gmail.com>
2026-01-12 20:28:12 +0100

    Add Two Numbers with Pointers

--- Programming Using C++ - Level 2/Exrcices/Pointers/addpftwo_nbr.c
+++ Programming Using C++ - Level 2/Exrcices/Pointers/addpftwo_nbr.c
@@ -6,22 +6,28 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/01/12 20:07:29 by shobeedev               shobee4ever      */
-/*   Updated: 2026/01/12 20:17:16 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/01/12 20:27:42 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+int addpftwo_nbr(int *n1,int *n2)
+{
+	return *n1 + *n2;
+}
+
 int main()
 {
 	int n1 = 5;
 	int n2 = 6;
+
 	int *num1,*num2;
 
 	num1 = &n1;
 	num2 = &n2;
 
-	int sum = *num1 + *num2;
+	int sum = addpftwo_nbr(num1,num2);
 	printf("%d\n",sum);
 
 }
