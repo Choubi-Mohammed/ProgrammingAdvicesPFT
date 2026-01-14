a1ce2a05a04601c7339e30697ff6d9a8849d2334
shobeedev <moha.saoud22@gmail.com>
2026-01-14 17:33:25 +0100

    vector - change element

--- Programming Using C++ - Level 2/Vectors Part I/vector_change_element.cpp
+++ Programming Using C++ - Level 2/Vectors Part I/vector_change_element.cpp
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/01/14 17:19:07 by shobeedev               shobee4ever      */
-/*   Updated: 2026/01/14 17:26:48 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/01/14 17:33:13 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ int main()
 	vector <int> vec{42,1337,22,23};
 
 	cout << "vector element : ";
-	for(const int i : vec)
+	for(const int &i : vec)
 		cout << i << " ";
 	cout << endl;
 
 	cout << "vector element after change i : ";
-	for(int i : vec)
+	for(int &i : vec)
 	{
 		i = 77;
 		cout << i << " ";
@@ -36,7 +36,7 @@ int main()
 	vec[1] = 1337;
 	vec[2] = 22;
 	vec.at(3) = 23;
-	for(const int i : vec)
+	for(const int &i : vec)
 		cout << i << " ";
 	cout << endl;
 
