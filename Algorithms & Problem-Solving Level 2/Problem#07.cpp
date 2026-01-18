15c11cc665ad9f1a8af38a7003e2970c56ecbfdc
shobeedev <moha.saoud22@gmail.com>
2026-01-18 16:10:44 +0100

    Transpose number in matrix

--- Algorithms & Problem-Solving Level 2/Problem#07.cpp
+++ Algorithms & Problem-Solving Level 2/Problem#07.cpp
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/01/18 15:23:42 by shobeedev               shobee4ever      */
-/*   Updated: 2026/01/18 15:56:23 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/01/18 16:10:29 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,25 +29,22 @@ void Ordered_Nbr(int arr[][100],int lenRow,int lenCol)
 		i++;
 	}
 }
-void PrintTransposeArr(int arr[][100],int lenRow,int lenCol)
+void TransposeArr(int arr[][100],int SecArr[][100],int lenRow,int lenCol)
 {
-	printf("\nthe following is a the transposeed matrix :\n");
 	int i = 0;
-	while(i < lenCol)
+	while(i < lenRow)
 	{
 		int j = 0;
-		while(j < lenRow)
+		while(j < lenCol)
 		{
-			printf("%d\t",arr[j][i]);
+			SecArr[i][j] = arr[j][i];
 			j++;
 		}
-		printf("\n");
 		i++;
 	}
 }
 void Print2DArr(int arr[][100],int lenRow,int lenCol)
 {
-	printf("the following is a %dx%d ordered number:\n",lenRow,lenCol);
 	int i =0;
 	while(i < lenRow)
 	{
@@ -66,13 +63,16 @@ int main()
 {
 	int arr[100][100];
 	int lenRow,lenCol;
-
+	int SecArr[100][100];
 	printf("Enter The len of Row and Col: ");
 	scanf("%d %d",&lenRow,&lenCol);
 
 	Ordered_Nbr(arr,lenRow,lenCol);
+	printf("the following is a %dx%d ordered number:\n",lenRow,lenCol);
 	Print2DArr(arr,lenRow,lenCol);
-	PrintTransposeArr(arr,lenRow,lenCol);
+	printf("\nthe following is a the transposeed matrix :\n");
+	TransposeArr(arr,SecArr,lenRow,lenCol);
+	Print2DArr(SecArr,lenRow,lenCol);
 	return 0;
 }
 
