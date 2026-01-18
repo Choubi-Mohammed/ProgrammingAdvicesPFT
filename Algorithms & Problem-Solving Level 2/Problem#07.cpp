32756fbb2e35b2e95460e1aef590a7fdef433bb7
shobeedev <moha.saoud22@gmail.com>
2026-01-18 15:58:03 +0100

    Transpose number in matrix

---
+++ Algorithms & Problem-Solving Level 2/Problem#07.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Problem#07.cpp                                         |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/01/18 15:23:42 by shobeedev               shobee4ever      */
+/*   Updated: 2026/01/18 15:56:23 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdio>
+using namespace std;
+
+void Ordered_Nbr(int arr[][100],int lenRow,int lenCol)
+{
+	int i = 0;
+	int counter = 1;
+	while(i < lenRow)
+	{
+		int j = 0;
+		while(j < lenCol)
+		{
+			arr[i][j] = counter;
+			counter++;
+			j++;
+		}
+		i++;
+	}
+}
+void PrintTransposeArr(int arr[][100],int lenRow,int lenCol)
+{
+	printf("\nthe following is a the transposeed matrix :\n");
+	int i = 0;
+	while(i < lenCol)
+	{
+		int j = 0;
+		while(j < lenRow)
+		{
+			printf("%d\t",arr[j][i]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+}
+void Print2DArr(int arr[][100],int lenRow,int lenCol)
+{
+	printf("the following is a %dx%d ordered number:\n",lenRow,lenCol);
+	int i =0;
+	while(i < lenRow)
+	{
+		int j = 0;
+		while(j < lenCol)
+		{
+			printf("%d\t",arr[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+}
+
+int main()
+{
+	int arr[100][100];
+	int lenRow,lenCol;
+
+	printf("Enter The len of Row and Col: ");
+	scanf("%d %d",&lenRow,&lenCol);
+
+	Ordered_Nbr(arr,lenRow,lenCol);
+	Print2DArr(arr,lenRow,lenCol);
+	PrintTransposeArr(arr,lenRow,lenCol);
+	return 0;
+}
+
