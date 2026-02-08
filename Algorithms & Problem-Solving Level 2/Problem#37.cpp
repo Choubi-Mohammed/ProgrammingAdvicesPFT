4cdf6838ed97344aa89a709a391832af4524f01a
shobeedev <moha.saoud22@gmail.com>
2026-02-08 12:39:29 +0100

    string manipulation - Split Word in Sentence

---
+++ Algorithms & Problem-Solving Level 2/Problem#37.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   Problem#37.cpp                                         |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/02/08 09:19:43 by shobeedev               shobee4ever      */
+/*   Updated: 2026/02/08 12:37:51 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+// welcome to lose boise hermanos // sep = space
+bool isSpace(char lett)
+{
+	return (lett == ' ' || lett == '\t' || lett == '\n' || lett == '\v' );
+}
+
+vector<string> spliteWord(string str,char sep)
+{
+	vector<string> vString;
+	int i = 0;
+	string word = "";
+	bool inword = false;
+	while(i < str.size())
+	{
+		if(str[i] != sep)
+			word+=str[i];
+		else //if(str[i] == sep)
+		{
+			if(word != "")
+			{
+				vString.push_back(word);
+				word = "";
+			}
+		}
+		i++;
+	}
+	if(word != "")
+		vString.push_back(word);
+	return vString;
+}
+
+int main()
+{
+	string str;
+	char sep;
+	cout << "Enter a Sentence : ";
+	getline(cin,str);
+	cout << "Enter a separator : ";
+	cin >> sep;
+	vector vString = spliteWord(str,sep);
+	for(string str : vString)
+		cout << str << endl;
+
+	return 0;
+}
